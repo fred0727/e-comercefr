f17e2850742905b0e875e348b47805032f6c67e6
fred0727 <freddymh07952023@gmail.com>
2023-06-29 22:03:36 -0500

    Updating project

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,20 +7,34 @@ const Header = () => {
   const dispatch = useDispatch();
 
   const handleClickShowCart = () => {
-    dispatch(changeIsShowCart())
+    dispatch(changeIsShowCart());
   };
 
   return (
     <header className="fixed bg-white w-full flex justify-between items-center py-4 px-4 z-10 lg:shadow-md lg:py-0 lg:h-[68px]">
-      <Link to="/" className="-text--primary font-bold text-2xl lg:text-3xl lg:pl-4">e-commerce</Link>
+      <Link
+        to="/"
+        className="-text--primary font-bold text-2xl lg:text-3xl lg:pl-4"
+      >
+        e-commerce
+      </Link>
       <nav className="flex justify-center items-center gap-5 lg:w-[600px]">
-        <Link to="/login" className="flex justify-center items-center border-l-[1px] lg:w-full lg:h-[68px]">
+        <Link
+          to="/login"
+          className="flex justify-center items-center lg:border-l-[1px] lg:w-full lg:h-[68px]"
+        >
           <i className="bx bx-user -text--text-gray text-[24px] lg:text-[28px]"></i>
         </Link>
-        <Link to="/purchases"  className="flex justify-center items-center border-l-[1px] lg:w-full lg:h-[68px]"> 
+        <Link
+          to="/purchases"
+          className="flex justify-center items-center  lg:border-l-[1px] lg:w-full lg:h-[68px]"
+        >
           <i className="bx bx-box -text--text-gray text-[24px] lg:text-[28px]"></i>
         </Link>
-        <button onClick={handleClickShowCart}  className="flex justify-center items-center border-l-[1px] lg:w-full lg:h-[68px]">
+        <button
+          onClick={handleClickShowCart}
+          className="flex justify-center items-center  lg:border-l-[1px] lg:w-full lg:h-[68px]"
+        >
           <i className="bx bx-cart -text--text-gray text-[25px] lg:text-[28px]"></i>
         </button>
       </nav>
