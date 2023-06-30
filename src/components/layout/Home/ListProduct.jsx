e9307b57dbe54640ea9524efb5ed94e03c950d78
fred0727 <freddymh07952023@gmail.com>
2023-06-30 08:39:24 -0500

    Updating changes ListProduct

--- src/components/layout/Home/ListProduct.jsx
+++ src/components/layout/Home/ListProduct.jsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 
 const ListProduct = ({ products }) => {
   return (
-    <section className="grid gap-10 p-2 grid-cols-[repeat(auto-fill,_275px)] md:grid-cols-[repeat(auto-fill,_325px)] lg:grid-cols-[repeat(auto-fill,_275px)] justify-between lg:mb-5">
+    <section className="grid gap-10 p-2 grid-cols-[repeat(auto-fill,_275px)] md:grid-cols-[repeat(auto-fill,_325px)] lg:grid-cols-[repeat(auto-fill,_275px)] justify-center md:justify-between lg:mb-5">
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
